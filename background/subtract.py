79d9c32bad8d3527e7274ca8b37779428596ec5c
Yuan Fu <casouri@gmail.com>
2018-02-13 14:50:48 -0500

    New background subtraction

--- background/subtract.py
+++ background/subtract.py
@@ -1,31 +1,64 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 import cv2
 
 IMG_PATH = 'small.png'
-img = cv2.imread(IMG_PATH, 0)
-bg = cv2.imread("bg.png", 0)
+img = cv2.imread(IMG_PATH, 1)
+bg = cv2.imread("bg.png", 1)
 
 threshold = 73
 maxValue = 225
 shape = (5, 5)
 
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+kernel = np.ones(shape, np.uint8)
 
-diff = cv2.absdiff(img, bg)
-# mask = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-imask = diff > 0
+# convert BGR to RGB
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+bg = cv2.cvtColor(bg, cv2.COLOR_BGR2RGB)
 
-blur = cv2.GaussianBlur(img, shape, 0)
+# blur
+img = cv2.GaussianBlur(img, shape, 0)
+bg = cv2.GaussianBlur(bg, shape, 0)
 
-ret, thresh1 = cv2.threshold(blur, threshold, maxValue, cv2.THRESH_BINARY_INV)
 
-plt.subplot(221)
-plt.imshow(blur)
-plt.subplot(222)
-plt.imshow(thresh1)
-plt.subplot(223)
-plt.hist(blur)
-plt.subplot(224)
-plt.imshow(imask)
+# subtract
+front = img - bg
+
+# combine channel
+r, g, b = cv2.split(front)
+combined = r + g + b
+
+# threshold
+ret, thresh_my = cv2.threshold(combined, threshold, maxValue, cv2.THRESH_BINARY)
+
+
+# close holes
+dilation = cv2.dilate(thresh_my, kernel, iterations=2)
+erosion = cv2.erode(dilation, kernel, iterations=2)
+
+# remove shadow
+erosion2 = cv2.erode(dilation, kernel, iterations=5)
+dilation2 = cv2.dilate(erosion2, kernel, iterations=3)
+
+# mask
+final_img = cv2.bitwise_or(img, img, mask = dilation2)
+
+plt.subplot(231)
+plt.title("original")
+plt.imshow(img)
+plt.subplot(232)
+plt.title("subtraction")
+plt.imshow(front)
+plt.subplot(233)
+plt.title("threshold")
+plt.imshow(thresh_my)
+plt.subplot(234)
+plt.title("remove hole")
+plt.imshow(erosion)
+plt.subplot(235)
+plt.title("remove shadow")
+plt.imshow(dilation2)
+plt.subplot(236)
+plt.title("mask")
+plt.imshow(final_img)
 plt.show()
